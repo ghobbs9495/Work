90bfb4c0672f6f7ef1a0f49526fdfe377d4ada67
Gary Hobbs <ghobbs9495@gmail.com>
2015-01-24 15:04:45 -0500

    First

---
+++ cachematrix.R
@@ -0,0 +1,25 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+    A = matrix(D,3,3)
+    A
+    
+    det(A)
+    solve(A)
+    #this is a test
+    #this is another test
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
+
+
+
+# C:\Users\ghobbs\Documents\GitHub\ProgrammingAssignment2
